51911c9268e0540166ab9d61a53282c9dd132517
rafaelcmoura <rafael.xuxubelez@gmail.com>
2020-05-17 15:46:13 -0300

    Changes print to use utils

--- src/unit_tests.c
+++ src/unit_tests.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include "unit_tests.h"
-
+#include "utils.h"
 
 void assert_array(int *array, int *expected_array, int size){
 
     for (int i = 0; i < size; i++) {
         if(array[i]!=expected_array[i]){
             printf("\t[\033[0;31mfailed\033[0m]");
-            printf("\nProvided array: [");
-            for (int j = 0; j < size; j++) {
-                printf("%d", array[j]);
-            }
-            printf("]");
-            printf("\nExpected array: [");
-            for (int j = 0; j < size; j++) {
-                printf("%d", expected_array[j]);
-            }
-            printf("]\n");
+            printf("\nProvided array: ");
+            print(array, size);
+            printf("\nExpected array: ");
+            print(expected_array, size);
             return;
         }
     }
