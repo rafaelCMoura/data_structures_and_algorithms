f00682cde6f5f5843d41ed3a1b0f0b3982541c18
rafaelcmoura <rafael.xuxubelez@gmail.com>
2020-05-17 15:44:55 -0300

    Adds print array function

--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
 #include "utils.h"
+
+void print(int *array, int size){
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        if(i < size-1){
+            printf("%d", array[i]);
+            printf(", ");
+        } else {
+            printf("%d", array[i]);
+            printf("]\n");
+        }
+    }
+}
